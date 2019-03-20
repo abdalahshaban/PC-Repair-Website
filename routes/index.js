8939c8afcbbd155317751dc1e489a57d2ea073e4
AbdullahShaaban <abdalahshaban@yahoo.com>
2019-03-21 00:51:40 +0200

    b

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var router = express.Router();
+var indexRouter = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+indexRouter.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    name: 'abdalah'
+  });
 });
 
-module.exports = router;
+module.exports = indexRouter;
